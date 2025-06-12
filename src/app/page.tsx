ebf30a0f828a71aa30a18532732606895e330a17
Joshua Coquia <joshua.coquia@hotmail.com>
2025-06-12 15:50:08 -0700

    Feat: create new task form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
-   <div>
-      <p>Hello World!</p>
-   </div> 
+   <main>
+    <h1 className="hidden">Effortless Tasks</h1>
+    {/* Add Task Form */}
+    <form>
+      <button type="submit" className="bg-blue-500 text-foreground rounded-md p-2 mr-2">Add Task</button>
+      <label htmlFor="newTaskTitle" className="hidden">Task List to Select</label>
+      <input type="text" name="newTaskTitle" id="newTaskTitle" placeholder="New Task Title" className="border-2 border-gray-300 rounded-md p-2" autoFocus />
+      <label htmlFor="taskListMenu" className="hidden">Task List to Select</label>
+      <select name="taskListMenu" id="taskListMenu" className="border-2 border-gray-300 rounded-md p-2 mt-4">
+
+      </select>
+    </form>
+    {/* Dropdown menu to select task list */}
+    {/* Task list */}
+   </main> 
   );
 }
