7479a4337a054930b5de0375029953cc2d52cc4c
Joshua Coquia <joshua.coquia@hotmail.com>
2025-06-29 14:30:14 -0700

    refactor: move account button into separate component

---
+++ src/components/AccountDropdown.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import accountCircleIcon from "@/images/account_circle_24dp_090B0D_FILL0_wght400_GRAD0_opsz24.svg";
+import type { Session } from "next-auth";
+
+type AccountDropdownProps = {
+    session: Session;
+};
+
+export default function AccountDropdown({session}: AccountDropdownProps) {
+    return (
+        <a href="#account" className="flex outline-0 h-12 border-2 border-grey-dark rounded-lg justify-center items-center pl-3 pr-2">
+            <span>{session.user.email}</span>
+            <Image src={accountCircleIcon} width={24} height={24} alt="Account Icon" className="ml-2 pointer-events-none" />
+        </a>
+    );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,14 @@
 import loginIcon from "@/images/login_24dp_090B0D_FILL0_wght400_GRAD0_opsz24.svg";
-import accountCircleIcon from "@/images/account_circle_24dp_090B0D_FILL0_wght400_GRAD0_opsz24.svg";
 import Image from "next/image";
 import { auth } from "@/auth";
+import AccountDropdown from "./AccountDropdown";
 
 export default async function Header() {
   const session = await auth();
   return (
     <header className="flex justify-end items-end p-4">
-      {session?.user ? (
-        <button className="flex outline-0 h-12 border-2 border-grey-dark rounded-lg justify-center items-center pl-3 pr-2">
-          <span>{session.user.email}</span>
-          <Image src={accountCircleIcon} width={24} height={24} alt="Account Icon" className="ml-2 pointer-events-none" />
-          {/* dropdown menu for account settings, sign out, etc. */}
-        </button>
+      {session ? (
+        <AccountDropdown session={session} />
       ) : (
         <a href="/login" className="w-12 h-12 border-2 rounded-lg border-grey-dark flex-none outline-0 hover:bg-grey-light-background focus:bg-grey-light-background transition-colors duration-150 flex justify-center" aria-label={`Open sign in modal`}>
           <Image src={loginIcon} width={24} height={24} alt="Login Icon" className="pointer-events-none" />
