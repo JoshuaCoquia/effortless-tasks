01e1f14c77a3144257077754688a9cca94ed4620
Joshua Coquia <joshua.coquia@hotmail.com>
2025-07-21 22:19:06 -0700

    feat: put sign up in separate page from login

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next'
+import { signup } from './actions'
+
+export const metadata: Metadata = {
+  title: 'Sign Up - Effortless Tasks',
+  description: 'Sign Up to Effortless Tasks to manage your to-do lists.',
+  openGraph: {
+    title: 'Sign Up - Effortless Tasks',
+    type: 'website',
+    url: 'https://effortless-tasks.vercel.app/Sign Up'
+  }
+}
+
+export default function SignUpPage() {
+  return (
+    <>
+      <section className="flex flex-col items-center justify-center">
+        <h1 className="mt-8 text-center font-bold text-md">Sign Up</h1>
+        <form className="my-4 mx-4 items-center flex flex-col gap-4 w-full max-w-prose">
+          <label className="font-bold w-full">Email<span className="text-red-dark">*</span>
+            <input id="email" name="email" type="email" required autoFocus className="w-full bg-grey-light-background border-0 rounded-lg my-0.5 px-2 py-1.5 font-normal" />
+            <p className="font-normal text-sm md:text-base">You will be sent an email to confirm your email address.</p>
+          </label>
+          <label className="font-bold w-full">Password<span className="text-red-dark">*</span>
+            <input id="password" name="password" type="password" required className="w-full bg-grey-light-background border-0 rounded-lg my-0.5 px-2 py-1.5 font-normal" />
+          </label>
+          <br />
+          <button formAction={signup} className="w-full border-2 border-black p-2 outline-0 rounded-lg cursor-pointer hover:bg-grey-light-background focus:bg-grey-light-background transition-colors duration-150">Sign Up</button>
+        </form>
+        <p>Already have an account? <a href="/login" className="underline">Login here.</a></p>
+      </section>
+    </>
+  )
+}
